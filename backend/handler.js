d12d85bc985b489e6ee60baee212775559e22844
Hongyi Nie <sylvia@Sylvias-MBP.cable.rcn.com>
2021-04-25 23:00:35 -0400

    Assignment 4 create and use API

--- backend/handler.js
+++ backend/handler.js
@@ -15,3 +15,44 @@ module.exports.hello = async (event) => {
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
+module.exports.ratings = async (event) => {
+  if (event.httpMethod === 'GET' && event.path === '/ratings'){
+    // check the header named Authorization
+    // const token = event.headers['Authorization']
+    // // If no token is provided, or it is "", return a 401
+    // if (!token) {
+    //   return {
+    //     statusCode: 401
+    //   }
+    // }
+
+    // try {
+    //   // validate the token from the request
+    //   const decoded = await firebaseTokenVerifier.validate(token, projectId)
+    // } catch (err) {
+    //   // the token was invalid, 
+    //   console.error(err)
+    //   return {
+    //     statusCode: 401
+    //   }
+    // }
+    return {
+      statusCode: 200,
+      headers:{
+            "Access-Control-Allow-Origin": '*',
+            "Content-Type": "*/*"
+      },
+      body: JSON.stringify(
+        {
+          film: 'harry potter',
+          time: '2000',
+          region: 'UK',
+          rating: '9.0', 
+          comments: '1000+'
+        },
+        null,
+        2
+      ),
+    };
+  }
+};
